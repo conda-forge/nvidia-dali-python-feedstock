4c74a0b77c8222d58ef729de61be03479fa8404f
Daniel Ching <dching@nvidia.com>
2025-04-28 20:07:41 -0500

    BLD: Provide default value for arm_variant_type

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,8 @@
 [[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
 [[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
 # https://docs.nvidia.com/cuda/cuda-compiler-driver-nvcc/index.html?highlight=tegra#cross-compilation
-[[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "sbsa" ]] && targetsDir="targets/sbsa-linux"
-[[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "tegra" ]] && targetsDir="targets/aarch64-linux"
+[[ ${target_platform} == "linux-aarch64" && ${arm_variant_type:-"sbsa"} == "sbsa" ]] && targetsDir="targets/sbsa-linux"
+[[ ${target_platform} == "linux-aarch64" && ${arm_variant_type:-"sbsa"} == "tegra" ]] && targetsDir="targets/aarch64-linux"
 
 if [ -z "${targetsDir+x}" ]; then
     echo "target_platform: ${target_platform} is unknown! targetsDir must be defined!" >&2
@@ -59,7 +59,7 @@ DALI_LINKING_ARGS=(
 )
 
 # Debug with fewer archs for shorter build times
-export CUDAARCHS="50"
+# export CUDAARCHS="50"
 
 # https://docs.nvidia.com/deeplearning/dali/user-guide/docs/compilation.html#optional-cmake-build-parameters
 cmake ${CMAKE_ARGS} \
